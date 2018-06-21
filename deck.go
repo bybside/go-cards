8a5a473d4514344f4780d4e062f631fb7d8c4771
bybside <bybside@gmail.com>
2018-06-20 23:18:27 -0500

    updated main.go and deck.go to initialize a deck of cards and write the deck to a file

--- deck.go
+++ deck.go
@@ -1,13 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // create a new type of 'deck' which is a slice of strings
 type deck []string
+
+// roughly equivalent to a constructor
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, value := range cardValues {
+		for _, suit := range cardSuits {
+			cards = append(cards, value + " of "+ suit)
+		}
+	}
+
+	return cards
+}
 
 // function with 'receiver' deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+// instantiate a new hand with cards drawn from deck
+func deal(d deck, handSize int) (deck, deck) {
+	hand := d[:handSize]
+	remainingDeck := d[handSize:]
+	return hand, remainingDeck
+}
+
+func (d deck) toString() string {
+	// takes a string slice and joins it into a single, comma delimited string
+	return strings.Join([]string(d),",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	// ioutil.WriteFile() writes a file to disk and takes
+	// a file name, a byte slice of data, and permissions
+	// for the file being written as arguments;
+	// if an error occurs, returns it
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
 }
--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()} // slice of type string
-	cards = append(cards, "Six of Spades")
-	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.saveToFile("deck.txt")
 }
